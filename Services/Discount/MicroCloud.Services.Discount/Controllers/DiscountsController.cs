fe8161704b855c750b0a03a0fa93ed1f04191e51
mehmettgunerr <63156838+mehmettgunerr@users.noreply.github.com>
2022-03-19 21:38:03 +0300

    Add Discount Api

---
+++ Services/Discount/MicroCloud.Services.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,66 @@
+﻿using MicroCloud.Services.Discount.Services;
+using MicroCloud.Shared.ControllerBases;
+using MicroCloud.Shared.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MicroCloud.Services.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : CustomBaseController
+    {
+        private readonly IDiscountService _discountService;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public DiscountsController(IDiscountService discountService, ISharedIdentityService sharedIdentityService)
+        {
+            _discountService = discountService;
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var discounts = await _discountService.GetAll();
+
+            return CreateActionResultInstance(discounts);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var discount = await _discountService.GetById(id);
+
+            return CreateActionResultInstance(discount);
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/GetByCode/{code}")]
+        public async Task<IActionResult> GetByCode(string code)
+        {
+            var discount = await _discountService.GetByCodeAndUserId(code, _sharedIdentityService.GetUserId);
+
+            return CreateActionResultInstance(discount);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(Models.Discount discount)
+        {
+            return CreateActionResultInstance(await _discountService.Save(discount));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Models.Discount discount)
+        {
+            return CreateActionResultInstance(await _discountService.Update(discount));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return CreateActionResultInstance(await _discountService.Delete(id));
+        }
+    }
+}
