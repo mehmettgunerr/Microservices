5bb064cd7db6378e077458c455acb415cd048eb1
mehmettgunerr <63156838+mehmettgunerr@users.noreply.github.com>
2022-03-19 18:35:45 +0300

    add basket.api

---
+++ Services/Basket/MicroCloud.Services.Basket/Controllers/BasketsController.cs
@@ -0,0 +1,44 @@
+﻿using MicroCloud.Services.Basket.Dtos;
+using MicroCloud.Services.Basket.Services;
+using MicroCloud.Shared.ControllerBases;
+using MicroCloud.Shared.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MicroCloud.Services.Basket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : CustomBaseController
+    {
+        private readonly IBasketService _basketService;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public BasketsController(IBasketService basketService, ISharedIdentityService sharedIdentityService)
+        {
+            _basketService = basketService;
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBasket()
+        {
+            return CreateActionResultInstance(await _basketService.GetBasket(_sharedIdentityService.GetUserId));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveOrBasket(BasketDto basketDto)
+        {
+            var response = await _basketService.SaveOrUpdate(basketDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBasket()
+        {
+            return CreateActionResultInstance(await _basketService.Delete(_sharedIdentityService.GetUserId));
+        }
+    }
+}
