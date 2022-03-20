3308d331a8afca2e94f4b10f0b48f1d63c708a43
mehmettgunerr <63156838+mehmettgunerr@users.noreply.github.com>
2022-03-20 15:28:08 +0300

    Add Order Api

---
+++ Services/Order/MicroCloud.Services.Order.Application/Commands/CreateOrderCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using MicroCloud.Services.Order.Application.Dtos;
+using MicroCloud.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroCloud.Services.Order.Application.Commands
+{
+    public class CreateOrderCommand:IRequest<Response<CreatedOrderDto>>
+    {
+        public string BuyerId { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+        public AddressDto Address { get; set; }
+    }
+}
